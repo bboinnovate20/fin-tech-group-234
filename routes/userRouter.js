0528bf04370e63b0c3d5176bfea420efa95077ec
Musa Ibrahim <bob@bboinnovate20@gmail.com>
2023-11-25 12:10:29 +0100

    change and add vercel.json

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,15 +10,15 @@ const userRouter = express.Router();
 userRouter.use(cookieParser())
 
 
-userRouter.post("/signup", middlewear.validateCreateUser, controller.createUser) 
+userRouter.post("/api/signup", middlewear.validateCreateUser, controller.createUser) 
 
 
-userRouter.post("/login",  controller.login)
+userRouter.post("/api/login",  controller.login)
 
-userRouter.post("/customer", auth,  controller.createCustomer)
-userRouter.get("/customer", auth,  controller.getCustomer)
+userRouter.post("/api/customer", auth,  controller.createCustomer)
+userRouter.get("/api/customer", auth,  controller.getCustomer)
 
-userRouter.post("/invoice", auth, controller.createInvoice)
-userRouter.get("/invoice", auth, controller.verifyInvoice)
+userRouter.post("/api/invoice", auth, controller.createInvoice)
+userRouter.get("/api/invoice", auth, controller.verifyInvoice)
 
 module.exports = userRouter
